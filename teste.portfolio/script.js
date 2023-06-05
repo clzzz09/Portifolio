1354a73d89fde9a9786f07011ca67e637eafe95c
clzzz09 <133062365+clzzz09@users.noreply.github.com>
2023-06-05 18:11:26 -0300

    Add files via upload

---
+++ teste.portfolio/script.js
@@ -0,0 +1,9 @@
+function sb_open() {
+    document.getElementById("mySidebar").style.display = "block"
+    document.getElementById("myOverlay").style.display = "block"
+}
+ 
+function sb_close() {
+    document.getElementById("mySidebar").style.display = "none"
+    document.getElementById("myOverlay").style.display = "none"
+}
